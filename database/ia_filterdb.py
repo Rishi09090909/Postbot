16bfb96d2dc369bfe5e61df6a6511cadcd42e922
Rk_botz <118276768+Rishi09090909@users.noreply.github.com>
2023-03-01 13:34:53 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -45,7 +45,6 @@ async def save_file(media):
             file_name=file_name,
             file_size=media.file_size,
             file_type=media.file_type,
-            mime_type=media.mime_type,
             caption=media.caption.html if media.caption else None,
         )
     except ValidationError:
